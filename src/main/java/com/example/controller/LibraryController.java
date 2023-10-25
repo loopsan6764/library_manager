def8978529f3a1486d235213dda7138d0739a5e0
Kohki Homma <loop.skydriver@hotmail.co.jp>
2023-10-25 13:43:02 +0900

    400エラーで貸し出しフォームの表示が出来ない

--- src/main/java/com/example/controller/LibraryController.java
+++ src/main/java/com/example/controller/LibraryController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.Library;
 import com.example.service.LibraryService;
@@ -27,5 +28,16 @@ public class LibraryController {
         List<Library> libraries = this.libraryService.findAll();
         model.addAttribute("libraries", libraries);
         return "library/index";
+    }
+    
+    @GetMapping("/borrow")
+    public String borrowingForm(@RequestParam("id") Integer id, Model model) {
+        // 書籍IDに基づいて書籍情報を取得する処理を実装する（未記述の部分）
+        Library library = libraryService.getLibraryById(id);
+        
+        // モデルに書籍情報を追加
+        model.addAttribute("library", library);
+        
+        return "borrowingForm"; // borrowingForm.html テンプレートを表示
     }
 }
